6bd9cc105eab594cc8699fe9b7fccf92f5cbd7e6
oscar phakathi <joeoscar77@gmailcom>
2023-06-15 12:54:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,7 @@
-#include <stdio.h> /*** main - prints the phase programming is like building multilingual puzzle *Return: 0 if exited properly, non-zero otherwise /*int main(void) {puts("\"Programming is like building a multilingual puzzle"); return (0); }
+#include <stdio.h>
+/**
+* main - prints the phase programming is like building multilingual puzzle 
+*Return: 0 if exited properly, non-zero otherwise 
+*/
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle"); return (0);}
