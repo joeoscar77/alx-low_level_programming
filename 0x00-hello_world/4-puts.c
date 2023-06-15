9c489709e3d3e432bb9ae0b36cfc8bbbb1be7ca5
oscar phakathi <joeoscar77@gmailcom>
2023-06-15 12:43:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,1 @@
-#include
+#include <stdio.h> /*** main - prints the phase programming is like building multilingual puzzle *Return: 0 if exited properly, non-zero otherwise /*int main(void) {puts("\"Programming is like building a multilingual puzzle"); return (0); }
